e4b4ba08e143617ad91e8a473e3f9e573587842d
Kharisov Damir <nsu.kharisov.damir@gmail.com>
2017-12-26 20:43:24 +0600

    view 0.2b

--- src/adobjects/User.java
+++ src/adobjects/User.java
@@ -20,4 +20,8 @@ public class User {
     public int getId() {
         return id;
     }
+
+    public int getType() {
+        return type;
+    }
 }
--- src/view/View.java
+++ src/view/View.java
@@ -15,11 +15,12 @@ import java.awt.event.FocusListener;
 
 public class View extends AbstractView {
     private static final int HEIGHT_PIXELS = 1000;
+    private static final int DATA_HEIGHT_PIXELS = 700;
     private static final int WIDTH_PIXELS = 500;
     private static final String[] USER_TYPES = {"customer", "shop"};
 
     private JFrame frame = new JFrame();
-    private JPanel panel = new JPanel();
+    private JPanel dataPanel = new JPanel();
     private JButton showLoginButton = new JButton("Login");
     private JButton showRegisterButton = new JButton("Register");
     private JTextField loginField = new HintTextField("Login", 30);
@@ -27,6 +28,13 @@ public class View extends AbstractView {
     private JComboBox<String> userTypeComboBox = new JComboBox<>(USER_TYPES);
     private JButton submitLoginButton = new JButton("Submit");
     private JButton submitRegisterButton = new JButton("Submit");
+    private JTextField errorField = new JTextField("FATAL ERROR!!!!!", 30);
+    private JTextField userType = new JTextField(30);
+    private boolean buttonsPanelAdded = false;
+    private JPanel buttonsPanel = new JPanel();
+    private JButton showMyProfileButton = new JButton("My profile");
+    private JTextField searchField = new HintTextField("Enter shop name", 20);
+    private JButton searchFieldButton = new JButton("Search");
 
     public View(AbstractControl control) {
         this.control = control;
@@ -37,57 +45,123 @@ public class View extends AbstractView {
         submitLoginButton.addActionListener(new submitLoginListener());
         submitRegisterButton.addActionListener(new submitRegisterListener());
         userTypeComboBox.setSelectedIndex(0);
+        errorField.setEditable(false);
+        showMyProfileButton.addActionListener(new showMyProfileListener());
+        searchFieldButton.addActionListener(new searchListener());
     }
 
     public void startSession() {
-        panel.setLayout(new GridBagLayout());
-        panel.setPreferredSize(new Dimension(WIDTH_PIXELS, HEIGHT_PIXELS));
-        frame.add(BorderLayout.CENTER, panel);
+        dataPanel.setLayout(new GridBagLayout());
+        dataPanel.setPreferredSize(new Dimension(WIDTH_PIXELS, HEIGHT_PIXELS));
+        frame.add(BorderLayout.CENTER, dataPanel);
         GridBagConstraints c = new GridBagConstraints();
         c.gridx = 0;
         c.gridy = 0;
         c.fill = GridBagConstraints.HORIZONTAL;
         c.gridwidth = 1;
-        panel.add(showLoginButton, c);
+        dataPanel.add(showLoginButton, c);
         c.gridy = 1;
-        panel.add(showRegisterButton, c);
+        dataPanel.add(showRegisterButton, c);
         frame.setVisible(true);
     }
 
     @Override
     public void update(Post[] data) {
-
     }
 
     @Override
     public void update(User data) {
-
-    }
-
-    @Override
-    public void update(User[] data) {
-
-    }
-
-    @Override
-    public void updateError() {
-
-    }
-
-    class showLoginListener implements ActionListener {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            panel.removeAll();
+        if (!buttonsPanelAdded) {
+            buttonsPanel.setLayout(new GridBagLayout());
+            buttonsPanel.setPreferredSize(new Dimension(WIDTH_PIXELS, HEIGHT_PIXELS - DATA_HEIGHT_PIXELS));
+            buttonsPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+            frame.add(BorderLayout.NORTH, buttonsPanel);
             GridBagConstraints c = new GridBagConstraints();
             c.gridx = 0;
             c.gridy = 0;
             c.fill = GridBagConstraints.HORIZONTAL;
             c.gridwidth = 1;
-            panel.add(loginField, c);
+            buttonsPanel.add(showMyProfileButton, c);
             c.gridy = 1;
-            panel.add(passwordField, c);
+            buttonsPanel.add(searchField, c);
+            c.gridx = 1;
+            buttonsPanel.add(searchFieldButton, c);
+            dataPanel.setPreferredSize(new Dimension(WIDTH_PIXELS, DATA_HEIGHT_PIXELS));
+            buttonsPanelAdded = true;
+        }
+        dataPanel.removeAll();
+        GridBagConstraints c = new GridBagConstraints();
+        c.gridx = 0;
+        c.gridy = 0;
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridwidth = 1;
+        loginField.setEditable(false);
+        loginField.setText(data.getLogin());
+        dataPanel.add(loginField, c);
+        c.gridy = 1;
+        userType.setEditable(false);
+        if (data.getType() == 0)
+            userType.setText("Customer");
+        else
+            userType.setText("Shop");
+        dataPanel.add(userType, c);
+        frame.setVisible(true);
+        frame.repaint();
+    }
+
+    @Override
+    public void update(User[] data) {
+        dataPanel.removeAll();
+        GridBagConstraints c = new GridBagConstraints();
+        c.gridx = 0;
+        c.gridy = 0;
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridwidth = 1;
+        for (User u : data) {
+            JTextField login = new JTextField(u.getLogin(), 30);
+            login.setEditable(false);
+            JTextField type = new JTextField(30);
+            type.setEditable(false);
+            if (u.getType() == 0)
+                type.setText("Customer");
+            else
+                type.setText("Shop");
+            dataPanel.add(login, c);
+            c.gridy++;
+            dataPanel.add(type, c);
+            c.gridy++;
+        }
+        frame.setVisible(true);
+        frame.repaint();
+    }
+
+    @Override
+    public void updateError() {
+        dataPanel.removeAll();
+        GridBagConstraints c = new GridBagConstraints();
+        c.gridx = 0;
+        c.gridy = 0;
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridwidth = 1;
+        dataPanel.add(errorField, c);
+        frame.setVisible(true);
+        frame.repaint();
+    }
+
+    class showLoginListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            dataPanel.removeAll();
+            GridBagConstraints c = new GridBagConstraints();
+            c.gridx = 0;
+            c.gridy = 0;
+            c.fill = GridBagConstraints.HORIZONTAL;
+            c.gridwidth = 1;
+            dataPanel.add(loginField, c);
+            c.gridy = 1;
+            dataPanel.add(passwordField, c);
             c.gridy = 2;
-            panel.add(submitLoginButton);
+            dataPanel.add(submitLoginButton);
             frame.setVisible(true);
             frame.repaint();
         }
@@ -96,19 +170,19 @@ public class View extends AbstractView {
     class showRegisterListener implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
-            panel.removeAll();
+            dataPanel.removeAll();
             GridBagConstraints c = new GridBagConstraints();
             c.gridx = 0;
             c.gridy = 0;
             c.fill = GridBagConstraints.HORIZONTAL;
             c.gridwidth = 1;
-            panel.add(loginField, c);
+            dataPanel.add(loginField, c);
             c.gridy = 1;
-            panel.add(passwordField, c);
+            dataPanel.add(passwordField, c);
             c.gridy = 2;
-            panel.add(userTypeComboBox, c);
+            dataPanel.add(userTypeComboBox, c);
             c.gridy = 3;
-            panel.add(submitLoginButton);
+            dataPanel.add(submitLoginButton);
             frame.setVisible(true);
             frame.repaint();
         }
@@ -125,6 +199,20 @@ public class View extends AbstractView {
         @Override
         public void actionPerformed(ActionEvent e) {
             control.register(loginField.getText(), passwordField.getText(),  Integer.parseInt((String) userTypeComboBox.getSelectedItem()));
+        }
+    }
+
+    class showMyProfileListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            control.showMyProfile();
+        }
+    }
+
+    class searchListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            control.search(searchField.getText());
         }
     }
 
