d748fbdd46b9e9a5aba69d179563f05af9e9a745
Kharisov Damir <nsu.kharisov.damir@gmail.com>
2017-12-26 18:31:20 +0600

    view started

--- src/view/View.java
+++ src/view/View.java
@@ -6,15 +6,47 @@ import adobjects.User;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 
 
 public class View extends AbstractView {
+    private static final int HEIGHT_PIXELS = 1000;
+    private static final int WIDTH_PIXELS = 500;
+    private static final String[] USER_TYPES = {"customer", "shop"};
+
     private JFrame frame = new JFrame();
+    private JPanel panel = new JPanel();
+    private JButton showLoginButton = new JButton("Login");
+    private JButton showRegisterButton = new JButton("Register");
+    private JTextField loginField = new HintTextField("Login", 30);
+    private JTextField passwordField = new HintTextField("Password", 30);
+    private JComboBox<String> userTypeComboBox = new JComboBox<>(USER_TYPES);
+    private JButton submitLoginButton = new JButton("Submit");
+    private JButton submitRegisterButton = new JButton("Submit");
+
+    public View() {
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setSize(WIDTH_PIXELS, HEIGHT_PIXELS);
+        showLoginButton.addActionListener(new showLoginListener());
+        showRegisterButton.addActionListener(new showRegisterListener());
+    }
 
     public void startSession() {
-        JTextField text = new JTextField("ASD");
-        //frame.add(text);
-        //text.setVisible(true);
+        panel.setLayout(new GridBagLayout());
+        panel.setPreferredSize(new Dimension(WIDTH_PIXELS, HEIGHT_PIXELS));
+        frame.add(BorderLayout.CENTER, panel);
+        GridBagConstraints c = new GridBagConstraints();
+        c.gridx = 0;
+        c.gridy = 0;
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridwidth = 1;
+        panel.add(showLoginButton, c);
+        c.gridy = 1;
+        panel.add(showRegisterButton, c);
+        frame.setVisible(true);
     }
 
     @Override
@@ -35,5 +67,106 @@ public class View extends AbstractView {
     @Override
     public void updateError() {
 
+    }
+
+    class showLoginListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            panel.removeAll();
+            GridBagConstraints c = new GridBagConstraints();
+            c.gridx = 0;
+            c.gridy = 0;
+            c.fill = GridBagConstraints.HORIZONTAL;
+            c.gridwidth = 1;
+            panel.add(loginField, c);
+            c.gridy = 1;
+            panel.add(passwordField, c);
+            frame.setVisible(true);
+            frame.repaint();
+        }
+    }
+
+    class showRegisterListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            panel.removeAll();
+            GridBagConstraints c = new GridBagConstraints();
+            c.gridx = 0;
+            c.gridy = 0;
+            c.fill = GridBagConstraints.HORIZONTAL;
+            c.gridwidth = 1;
+            panel.add(loginField, c);
+            c.gridy = 1;
+            panel.add(passwordField, c);
+            c.gridy = 2;
+            panel.add(userTypeComboBox, c);
+            frame.setVisible(true);
+            frame.repaint();
+        }
+    }
+
+    class submitLoginListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    }
+
+    class showRegisterListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            panel.removeAll();
+            GridBagConstraints c = new GridBagConstraints();
+            c.gridx = 0;
+            c.gridy = 0;
+            c.fill = GridBagConstraints.HORIZONTAL;
+            c.gridwidth = 1;
+            panel.add(loginField, c);
+            c.gridy = 1;
+            panel.add(passwordField, c);
+            c.gridy = 2;
+            panel.add(userTypeComboBox, c);
+            frame.setVisible(true);
+            frame.repaint();
+        }
+    }
+
+    class HintTextField extends JTextField implements FocusListener {
+        private final String hint;
+        private boolean showingHint;
+
+        public HintTextField(final String hint) {
+            super(hint);
+            this.hint = hint;
+            this.showingHint = true;
+            super.addFocusListener(this);
+        }
+
+        public HintTextField(final String hint, int columns) {
+            super(hint, columns);
+            this.hint = hint;
+            this.showingHint = true;
+            super.addFocusListener(this);
+        }
+
+        @Override
+        public void focusGained(FocusEvent e) {
+            if( this.getText().isEmpty()) {
+                super.setText("");
+                showingHint = false;
+            }
+        }
+        @Override
+        public void focusLost(FocusEvent e) {
+            if (this.getText().isEmpty()) {
+                super.setText(hint);
+                showingHint = true;
+            }
+        }
+
+        @Override
+        public String getText() {
+            return showingHint ? "" : super.getText();
+        }
     }
 }
